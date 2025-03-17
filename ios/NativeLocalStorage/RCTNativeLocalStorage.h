38fb80c05acf5edeaa4e4d8313f4bff9c6b3a8c1
rahul.nainwal <rahul.nainwal@tothenew.com>
2025-03-17 11:25:49 +0530

    Changes for android LocalStorage turbo module.

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  RNTurboModulesDemo
+//
+//  Created by Rahul Nainwal on 17/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  RNTurboModulesDemo
+//
+//  Created by Rahul Nainwal on 17/03/25.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
